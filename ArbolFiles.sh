6f07f79fc13f89c77483711a1454616c3ff75d79
DianaRojasUwU <108961369+DianaRojasUwU@users.noreply.github.com>
2023-09-19 23:53:33 -0500

    Tarea 994 - Scripts
    bash

---
+++ ArbolFiles.sh
@@ -0,0 +1,16 @@
+clear
+cd ~/documents
+mkdir Diana
+cd ~/Documents/Diana
+mkdir uno
+mkdir dos
+mkdir tres
+mkdir tres/cuatro
+mkdir tres/cuatro/cinco
+mkdir seis 
+touch  uno/test.txt
+touch tres/file3.tx
+#touch seis/holamundo.sh
+#touch seis/holanombre.sh
+bash holamundo.sh
+bash holanombre.sh
